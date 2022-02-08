a2a0367e78b2d6c3acd774d3357f719e749e2af4
Zhenia <zhenia1985@gmail.com>
2022-02-08 14:23:45 +0700

    тоглер темы сделан

--- script/script.js
+++ script/script.js
@@ -1,1 +1,26 @@
 // сегодня тут так же, как у меня в холодильнике.
+
+const toggler = document.querySelectorAll(".toggler__checkbox")
+
+const header = document.querySelector(".header")
+const overview = document.querySelector(".overview")
+const place = document.querySelector(".place")
+const road = document.querySelector(".road")
+const bike = document.querySelector(".bike")
+const training = document.querySelector(".training")
+const footer = document.querySelector(".footer")
+
+
+// жмяклер темы
+toggler.forEach(checkbox => {
+   checkbox.addEventListener('change', function () {
+
+      header.classList.toggle("header_dark")
+      overview.classList.toggle("overview_dark")
+      place.classList.toggle("place_dark")
+      road.classList.toggle("road_dark")
+      bike.classList.toggle("bike_dark")
+      training.classList.toggle("training_dark")
+      footer.classList.toggle("footer_dark")
+   })
+})
