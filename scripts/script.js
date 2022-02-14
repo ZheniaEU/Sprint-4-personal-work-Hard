b807833f6d38a3983ee5545be7de1ce8f1e9be2f
Zhenia <zhenia1985@gmail.com>
2022-02-14 20:49:47 +0700

    бургер плавная прокрутка

--- scripts/script.js
+++ scripts/script.js
@@ -3,11 +3,13 @@
 const toggler = document.querySelectorAll(".toggler__checkbox")
 
 const burger = document.querySelector(".burger__box")
-const burgers = document.querySelector(".burgers")
 //кнопка формы мыла
 const emailForm = document.querySelector(".footer__form")
 const email = document.querySelector(".footer__email")
 const emailButton = document.querySelector(".footer__button-submit")
+
+//ссылки
+const navLinks = document.querySelectorAll(".header__links[data-goto]")
 
 const header = document.querySelector(".header")
 const overview = document.querySelector(".overview")
@@ -17,6 +19,10 @@ const bike = document.querySelector(".bike")
 const training = document.querySelector(".training")
 const footer = document.querySelector(".footer")
 
+// бургер
+burger.addEventListener("click", function () {
+   header.classList.toggle("header_active");
+})
 
 // жмяклер темы 
 toggler.forEach(checkbox => {
@@ -33,22 +39,42 @@ toggler.forEach(checkbox => {
 
 //кнопка мыла
 email.addEventListener("click", function () {
-   emailButton.classList.add("footer__button-submit_active");
+   emailButton.classList.add("footer__button-submit_active")
 })
 
 //кнопка формы
 emailButton.addEventListener("click", function () {
-   email.value = "Круто!";
-   emailButton.disabled = true;
-   email.disabled = true;
+   email.value = "Круто!"
+   emailButton.disabled = true
+   email.disabled = true
 })
 
 //кнопка формы
 emailForm.addEventListener("mouseleave", function () {
-   emailButton.classList.remove("footer__button-submit_active");
+   emailButton.classList.remove("footer__button-submit_active")
 })
 
-// бургер
-burger.addEventListener("click", function() {
-   header.classList.toggle("header_active");
- })
+
+// прокрутка ссылок
+
+if (navLinks.length > 0) {
+   navLinks.forEach(navLinks => {
+      navLinks.addEventListener("click", onMenuLinkClick)
+   })
+
+   function onMenuLinkClick(evt) {
+      const navLink = evt.target
+      if (navLink.dataset.goto && document.querySelector(navLink.dataset.goto)) {
+         const gotoBlock = document.querySelector(navLink.dataset.goto)
+         const gotoBlockValue = gotoBlock.getBoundingClientRect().top
+         // - document.querySelector("header").offsetHeight
+         // header.classList.remove("header_active")
+
+         window.scrollTo({
+            top: gotoBlockValue,
+            behavior: "smooth"
+         })
+         evt.preventDefault()
+      }
+   }
+}
